dcd65b1951d05eb26ee23845ac03798110d275bc
Irish Rixon Dela Pena <irishrixon@gmail.com>
2026-01-14 13:42:17 +0800

    `Removed axiosConfig and emailFormAPI files`

--- src/API/emailFormAPI.tsx
+++
@@ -1,6 +0,0 @@
-import type { emailContent } from "@/Interface/interface";
-import { axiosInstance } from "./axiosConfig";
-
-export const postEmailForm = (body: emailContent) => {
-    return axiosInstance.post("", body);
-};
